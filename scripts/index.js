042b310e6fed11c6fb65b052a1f920afa39175c9
fauxfox117 <SABolin1@me.com>
2025-04-22 16:03:09 -0400

    changes implemented

--- scripts/index.js
+++ scripts/index.js
@@ -14,8 +14,22 @@ const newPostForm = newPostModal.querySelector(".modal__form");
 const newPostLink = newPostModal.querySelector("#card-image-input");
 const newPostDescription = newPostModal.querySelector("#card-caption-input");
 
+/*      below is commented for future implementation
+
+
 const cardImage = document.querySelector(".card__image");
 const cardDescription = document.querySelector(".card__title");
+
+
+*/
+
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
 
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
@@ -23,26 +37,26 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 editProfileBtn.addEventListener("click", function () {
   editProfileName.value = profileNameEl.textContent;
   editProfileDescription.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileName.value;
   profileDescriptionEl.textContent = editProfileDescription.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal;
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
@@ -55,10 +69,16 @@ function handleNewPostSubmit(evt) {
   }
   console.log("New Post Link:", newPostLink.value);
   console.log("New Post Description:", newPostDescription.value);
+
+  /*      below is commented for future implementation
+
   cardImage.textContent = newPostLink.value;
   cardDescription.textContent = newPostDescription.value;
+
+  */
+
   newPostForm.reset();
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal;
 }
 
 newPostForm.addEventListener("submit", handleNewPostSubmit);
